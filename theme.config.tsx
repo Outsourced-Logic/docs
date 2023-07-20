4532aae7671030d6791afc1c2fcf0ca9b0dba8bc
Karthik Raja <karthikraja2k1@gmail.com>
2023-07-21 01:10:31 +0530

    add content pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router';
 
 const config: DocsThemeConfig = {
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Nextra" />
+      <meta property="og:description" content="The next site builder" />
+    </>
+    ),
+
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – MultiOn'
+      }
+    }
+    else{
+      return {
+        titleTemplate: 'MultiOn'
+      }
+    }
+  },
   logo: <span>MULTION AI</span>,
   project: {
     link: 'https://github.com/MULTI-ON/docs',
