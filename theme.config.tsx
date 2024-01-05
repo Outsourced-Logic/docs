6b0a8df34a3ecedd47662ce4938266b8ac3ee41a
Ahsan Khan <ahsankhanamu@gmail.com>
2024-01-05 22:41:18 +0530

    Update- New logo updated in pages and as fevicon.ico, also works with system color scheme

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,29 +2,22 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router';
 
-const logo = (
-  <svg height="32" viewBox="0 0 594 591" fill="none" style={{display: "inline-block"}} xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M383.123 552.415C458.468 526.944 481.207 391.222 433.526 250.184C385.846 109.146 299.198 11.0371 210.768 40.9324C126.527 69.4119 112.997 201.387 160.677 342.426C208.358 483.464 307.778 577.887 383.123 552.415ZM373.462 497.931C425.955 480.185 437.683 373.46 399.415 260.264C361.147 147.067 296.686 66.6073 235.077 87.4353C176.386 107.277 171.053 211.332 209.321 324.528C247.59 437.725 320.969 515.677 373.462 497.931Z" fill="url(#paint0_linear_321_7189)"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M545.614 190.124C511.515 118.271 374.067 111.543 239.564 175.373C105.061 239.203 17.7465 336.718 57.7673 421.05C95.8928 501.388 228.545 499.408 363.048 435.578C497.551 371.748 579.713 261.978 545.614 190.124ZM492.635 206.094C468.878 156.034 361.514 156.853 253.563 208.083C145.612 259.313 73.2331 332.731 101.116 391.485C127.678 447.456 231.643 440.598 339.594 389.368C447.545 338.138 516.392 256.155 492.635 206.094Z" fill="url(#paint1_linear_321_7189)"/>
-<path fill-rule="evenodd" clip-rule="evenodd" d="M155.8 325.689L196.485 283.265C196.485 283.265 192.681 268.67 189.453 249.371C184.738 222.385 183.095 197.152 184.704 174.978C184.784 171.916 184.866 170.16 184.866 170.16L133.934 170.703C130.941 216.313 138.45 270.002 155.8 325.689Z" fill="url(#paint2_linear_321_7189)"/>
-<defs>
-<linearGradient id="paint0_linear_321_7189" x1="182.591" y1="417.548" x2="461.763" y2="319.134" gradientUnits="userSpaceOnUse">
-<stop stop-color="#D7539E"/>
-<stop offset="0.6875" stop-color="#FA9891"/>
-<stop offset="1" stop-color="#F2F625"/>
-</linearGradient>
-<linearGradient id="paint1_linear_321_7189" x1="65.8662" y1="442.254" x2="507.491" y2="358.282" gradientUnits="userSpaceOnUse">
-<stop stop-color="#9895C2"/>
-<stop offset="0.457245" stop-color="#E74274"/>
-<stop offset="1" stop-color="#F6ED2D"/>
-</linearGradient>
-<linearGradient id="paint2_linear_321_7189" x1="183.67" y1="417.129" x2="458.311" y2="318.235" gradientUnits="userSpaceOnUse">
-<stop stop-color="#D7539E"/>
-<stop offset="0.6875" stop-color="#FA9891"/>
-<stop offset="1" stop-color="#F2F625"/>
-</linearGradient>
-</defs>
-</svg>)
+const logo = () => {
+  return (
+    <span>
+      <svg style={{ marginRight: 5, display: "inline-block" }} width="32" height="32" viewBox="0 0 400 400" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_26_2)">
+        <path fill="currentColor" d="M121.361 258.126C139.88 245.518 158.909 224.66 174.209 198.344C189.509 172.028 198.207 145.158 200 122.823L278.638 143.638C278.631 100.208 243.43 65 200 65C156.569 65 121.361 100.208 121.361 143.638V258.126ZM273.123 267.687C252.941 257.953 225.365 251.905 194.924 251.811C164.482 251.718 136.863 257.622 116.624 267.234L95.3335 188.725C57.7207 210.44 44.8327 258.536 66.5479 296.149C88.2631 333.762 136.359 346.65 173.972 324.934L273.123 267.687ZM304.666 188.725L205.515 131.478C207.178 153.819 215.725 180.726 230.866 207.135C246.008 233.545 264.929 254.511 283.376 267.234H283.383L226.035 324.927C263.648 346.642 311.744 333.754 333.459 296.142C355.167 258.529 342.279 210.44 304.666 188.725Z"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_26_2">
+        <rect width="288" height="270.72" fill="white" transform="translate(56 65)"/>
+        </clipPath>
+        </defs>
+      </svg>
+      <span>MULTION AI</span>
+    </span>
+  )}
 
 const config: DocsThemeConfig = {
   head: (
@@ -52,7 +45,7 @@ const config: DocsThemeConfig = {
     }
   },
  
-  logo: <span>{logo} MULTION AI</span>,
+  logo: logo,
   project: {
     link: 'https://github.com/MULTI-ON/api',
   },
